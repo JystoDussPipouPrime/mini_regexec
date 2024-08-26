09b4557ec13bfbebb028880a143ec8f8832b8a00
mcurto <mcurto@student.42lyon.fr>
2024-08-26 06:19:11 +0200

    add a-9

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mini_regexec
+
+int	mini_regexec(const char *text, const char *pattern)
+
+- [x] a-z
+- [x] a-Z
+- [x] A-Z
+- [x] 0-9
+
+
+- [ ] patterns segfault if not [a-z] / [a-Z0-9] / [A-Z0-9a-Z]
---
+++ miniregex.c
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniregex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdpp <jdpp@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 06:04:13 by jdpp              #+#    #+#             */
+/*   Updated: 2024/08/26 06:17:40 by jdpp             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef enum e_class
+{
+	c_null,
+	c_az,
+	c_AZ,
+	c_aZ,
+	c_09,
+	c_az09,
+	c_AZ09,
+	c_aZ09
+}	t_class;
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	j;
+	size_t	i;
+
+	i = 0;
+	j = 0;
+	if (little[i] == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		while (big[i + j] == little[j] && i + j < len)
+		{
+			j++;
+			if (little[j] == '\0')
+			{
+				return ((char *)big + i);
+			}
+		}
+		j = 0;
+		i++;
+	}
+	return (NULL);
+}
+
+bool	parse_text_one(const char *text, int (*ischeck)(int))
+{
+	size_t	i;
+
+	i = 0;
+	while (text[i])
+	{
+		if (!ischeck(text[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+bool	parse_text_two(const char *text, int (*ischeck)(int), int (*ischeck2)(int))
+{
+	size_t	i;
+
+	i = 0;
+	while (text[i])
+	{
+		if (!ischeck(text[i]) && !ischeck2(text[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+int	inside_bracket(const char *pattern, size_t *i)
+{
+	int	res;
+
+	res = 0;
+	(*i)++;
+	while (pattern[*i])
+	{
+		if (pattern[*i] && pattern[*i + 1] && pattern[*i + 2])
+		{
+			if (pattern[*i] != '[' && pattern[*i + 1] != '[' && pattern[*i + 2] != '[')
+			{
+				if (pattern[*i] != ']' && pattern[*i + 1] != ']' && pattern[*i + 2] != ']')
+				{
+					if (ft_strnstr(pattern + *i, "a-z", 3))
+						res += 1;
+					else if (ft_strnstr(pattern + *i, "A-Z", 3))
+						res += 2;
+					else if (ft_strnstr(pattern + *i, "0-9", 3))
+						res += 4;
+					(*i) += 3;
+				}
+			}
+		}
+		if (pattern[*i] == ']' )
+			return (res);
+	}
+	return (res);
+}
+
+int	mini_regexec(const char *text, const char *pattern)
+{
+	size_t	i;
+	int		class;
+
+	i = 0;
+	class = 0;
+	if (!text || !pattern)
+		return (-1);
+	if (pattern[i] == '[')
+		class = inside_bracket(pattern, &i);
+	if (class == c_az)
+		return (parse_text_one(text, islower));
+	else if (class == c_AZ)
+		return (parse_text_one(text, isupper));
+	else if (class == c_aZ)
+		return (parse_text_one(text, isalpha));
+	else if (class == c_09)
+		return (parse_text_one(text, isdigit));
+	else if (class == c_az09)
+		return (parse_text_two(text, isdigit, islower));
+	else if (class == c_AZ09)
+		return (parse_text_two(text, isdigit, isupper));
+	else if (class == c_aZ09)
+		return (parse_text_two(text, isdigit, isalpha));
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	const char	*patterns1 = "[a-za-ZA-Z0-9]";
+
+	if (argc != 2)
+		return (0);
+	printf("Pattern: %s\n", patterns1);
+	if (mini_regexec(argv[1], patterns1))
+		printf("Match found: %s\n", argv[1]);
+	else
+		printf("No match: %s\n", argv[1]);
+	return (0);
+}
+
+//const char *patterns2 = "[a-zA-Z0-9]+[@][a-z0-9]";
