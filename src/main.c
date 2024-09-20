5f035e508462e0f154228cf5bb053a8c8385a418
mcurto <mcurto@student.42lyon.fr>
2024-09-20 16:38:05 +0200

    //atoibase

--- src/main.c
+++ src/main.c
@@ -15,11 +15,11 @@
 int	main(int argc, char **argv)
 {
 	char	patterns1[4][100] = {"[0-9]", "0b[0-1]", "0[0-7]", "0x[a-f0-9]"};
-	char	base[4][100] = {"0123456789", "01", "01234567", "0123456789abcdef"};
+	// char	base[4][100] = {"0123456789", "01", "01234567", "0123456789abcdef"};
 	int		prefix;
 
 	if (argc != 2)
-		return (0);
+		return (printf(RED"./mini_regex <num>\n"CRESET), 0);
 	prefix = find_prefix(argv[1]);
 	ft_strtolower(argv[1]);
 	printf("PAT:\t%s\n", patterns1[prefix]);
@@ -27,6 +27,6 @@ int	main(int argc, char **argv)
 		printf(GRN"OK:\t%s\n"CRESET, argv[1]);
 	else
 		printf(RED"NOK:\t%s\n"CRESET, argv[1]);
-	printf("DEC:\t%d\n", ft_atoi_base_positive(argv[1], base[prefix]));
+	// printf("DEC:\t%d\n", ft_atoi_base_positive(argv[1], base[prefix]));
 	return (0);
 }
